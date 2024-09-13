a1bc6c5a7712c7ab7df32ceea73e630be4d28101
NoelEK-skol <noek07001@utb.vaxjo.se>
2024-09-13 13:55:45 +0200

    Påbörjat uppgiften

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 101); //slumpar ett tal mellan 1 och 100
+
+    Console.WriteLine("Gissa vilket tal jag tänker på mellan 1 och 100!");
+    string str = Console.ReadLine();
+    int number = Convert.ToInt32(str);
+
+do
+    {
+        str = Console.ReadLine();
+
+    if(number < randomNumber)
+        {
+            Console.WriteLine("Gissningen är för låg");
+        }
+    else if(number > randomNumber)
+        {
+            Console.WriteLine("Gissningen är för hög");
+        }
+    }
+    while (number == randomNumber);
+            Console.WriteLine("Rätt!");
+        
+        Console.ReadLine();
+}
